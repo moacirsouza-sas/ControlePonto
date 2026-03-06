0e109f4c382fc1d168755b4bb49228bb00ba87f5
moacirsouza-sas <moacirsouza@sas.sc.gov.br>
2026-03-06 15:24:41 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,223 +1,118 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyERIOLmvKu5jwniMUfjnPDNjpW6Zlkjzv430o7jah7VJNGc-9V7K6Cq16Q8_2rwHs/exec";
 
-const jornada = 480;
+let db = JSON.parse(localStorage.getItem("ponto_db") || "{}");
 
-let dados = JSON.parse(localStorage.getItem("dados")) || {dias:{}};
+let hoje = new Date().toLocaleDateString();
 
-let hoje = new Date().toLocaleDateString("pt-BR");
+if(!db[hoje]){
 
-if(!dados.dias[hoje]){
-
-dados.dias[hoje]={
-
-entrada:null,
-saidaAlmoco:null,
-voltaAlmoco:null,
-saida:null
-
+db[hoje] = {
+entrada:"",
+almocoSai:"",
+almocoVolta:"",
+saida:"",
+saldo:""
 };
 
 }
 
-function salvar(){
+function registrar(tipo){
 
-localStorage.setItem("dados",JSON.stringify(dados));
+let agora = new Date();
+let hora = agora.toLocaleTimeString();
 
-}
+db[hoje][tipo] = hora;
 
-function agora(){
+document.getElementById("horaRegistro").innerText = hora;
 
-return new Date().toLocaleTimeString("pt-BR");
-
-}
-
-function registrarEntrada(){
-
-dados.dias[hoje].entrada=agora();
-
-registrar("Entrada registrada");
-
-}
-
-function registrarSaidaAlmoco(){
-
-dados.dias[hoje].saidaAlmoco=agora();
-
-registrar("Saída almoço registrada");
-
-}
-
-function registrarVoltaAlmoco(){
-
-dados.dias[hoje].voltaAlmoco=agora();
-
-registrar("Volta almoço registrada");
-
-}
-
-function registrarSaida(){
-
-dados.dias[hoje].saida=agora();
-
-registrar("Saída registrada");
-
-}
-
-function registrar(msg){
-
-salvar();
-
-document.getElementById("status").innerText=msg;
-
-document.getElementById("horaRegistro").innerText=agora();
-
-calcularHoras();
+salvarLocal();
 
 enviarServidor();
 
-renderizarHistorico();
-
-if(typeof gerarGrafico==="function") gerarGrafico();
+atualizarTela();
 
 }
 
-function converter(h){
+function salvarLocal(){
 
-if(!h) return 0;
-
-let p=h.split(":");
-
-return parseInt(p[0])*60+parseInt(p[1]);
-
-}
-
-function calcularHoras(){
-
-let d=dados.dias[hoje];
-
-let entrada=converter(d.entrada);
-
-let saida=converter(d.saida);
-
-let almocoSai=converter(d.saidaAlmoco);
-
-let almocoVolta=converter(d.voltaAlmoco);
-
-let total=0;
-
-if(entrada && saida){
-
-total=saida-entrada;
-
-if(almocoSai && almocoVolta){
-
-total-=almocoVolta-almocoSai;
-
-}
-
-}
-
-let faltante=Math.max(jornada-total,0);
-
-let extra=Math.max(total-jornada,0);
-
-document.getElementById("horasFeitas").innerText=formatar(total);
-document.getElementById("horasFaltantes").innerText=formatar(faltante);
-document.getElementById("horasExtras").innerText=formatar(extra);
-
-}
-
-function formatar(m){
-
-let h=Math.floor(m/60);
-let min=m%60;
-
-return `${h}h ${min}m`;
+localStorage.setItem("ponto_db", JSON.stringify(db));
 
 }
 
 function enviarServidor(){
 
-let d=dados.dias[hoje];
+navigator.geolocation.getCurrentPosition(function(pos){
+
+let dados = {
+
+data: hoje,
+entrada: db[hoje].entrada,
+almocoSai: db[hoje].almocoSai,
+almocoVolta: db[hoje].almocoVolta,
+saida: db[hoje].saida,
+saldo: db[hoje].saldo,
+geo: pos.coords.latitude + "," + pos.coords.longitude
+
+};
 
 fetch(API_URL,{
 
 method:"POST",
-
-body:JSON.stringify({
-
-data:hoje,
-entrada:d.entrada,
-almocoSai:d.saidaAlmoco,
-almocoVolta:d.voltaAlmoco,
-saida:d.saida,
-saldo:0,
-geo:"gps"
+body:JSON.stringify(dados)
 
 })
-
-})
-
 .then(r=>r.text())
-.then(res=>console.log(res))
-.catch(err=>console.log(err));
+.then(r=>console.log(r));
+
+});
 
 }
 
-function renderizarHistorico(){
+function atualizarTela(){
 
-let tbody=document.querySelector("#tabela tbody");
+let hist = document.getElementById("historico");
 
-tbody.innerHTML="";
+hist.innerHTML="";
 
-Object.keys(dados.dias).forEach(data=>{
+for(let data in db){
 
-let d=dados.dias[data];
+let tr = document.createElement("tr");
 
-let tr=document.createElement("tr");
+tr.innerHTML = `
 
-tr.innerHTML=`
 <td>${data}</td>
-<td>${d.entrada||""}</td>
-<td>${d.saida||""}</td>
-<td>-</td>
+<td>${db[data].entrada}</td>
+<td>${db[data].saida}</td>
+<td>${db[data].saldo}</td>
+
 `;
 
-tbody.appendChild(tr);
+hist.appendChild(tr);
 
-});
+}
 
 }
 
 function exportarCSV(){
 
-let csv="Data,Entrada,Saida\n";
+let csv="Data,Entrada,Saida,Saldo\n";
 
-Object.keys(dados.dias).forEach(data=>{
+for(let data in db){
 
-let d=dados.dias[data];
-
-csv+=`${data},${d.entrada||""},${d.saida||""}\n`;
-
-});
-
-let blob=new Blob([csv],{type:"text/csv"});
-
-let a=document.createElement("a");
-
-a.href=URL.createObjectURL(blob);
-
-a.download="ponto.csv";
-
-a.click();
+csv += `${data},${db[data].entrada},${db[data].saida},${db[data].saldo}\n`;
 
 }
 
-window.onload=()=>{
+let blob = new Blob([csv],{type:"text/csv"});
 
-renderizarHistorico();
+let link = document.createElement("a");
 
-calcularHoras();
+link.href = URL.createObjectURL(blob);
 
-};
+link.download="ponto.csv";
 
+link.click();
+
+}
+
+atualizarTela();
