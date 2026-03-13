9c4e7bb239c90d5a53e97a849fc084a6a9a6dff2
souzamoacircalgaro-arch <souzamoacircalgaro@gmail.com>
2026-03-13 12:24:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API = "https://script.google.com/macros/s/AKfycbxptl7OigMrmAW9HOrHcuGdyy1sK7AA6I1M25nwWBKSnUWZ9qI9cnolWpeSw0kHxb4/exec";
+const API = "https://script.google.com/macros/s/AKfycbxvipB4oDADLdVXQ1bc9inPV-RjYyiDdrJmWSZ6cSJjRd5Na-LAgth5E9JmklWxgYA/exec";
 
 const PLANILHA="https://docs.google.com/spreadsheets/d/1ItfOyHZhqiZVQcaYIq4S3Dz4PLdeu_LRwNSXFLyw5sE/edit"
 
@@ -226,6 +226,7 @@ obterGPS()
 carregarHistorico()
 
 })
+
 
 
 
